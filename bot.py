24e426695852345e338967a041d4665dc123331c
candle71234 <102852420+candle71234@users.noreply.github.com>
2026-03-26 09:11:42 +0800

    修正env

--- bot.py
+++ bot.py
@@ -7,8 +7,8 @@ from discord.ext import commands
 from discord.ext import tasks
 
 # 本地用
-from dotenv import load_dotenv
-load_dotenv()
+# from dotenv import load_dotenv
+# load_dotenv()
 
 # 抓時間 & 設定時區
 from datetime import time, datetime, timedelta
