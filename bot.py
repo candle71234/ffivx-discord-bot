d285dd11c958fc562c86e285b50189d3dd3f3012
candle71234 <102852420+candle71234@users.noreply.github.com>
2026-03-26 08:32:22 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -44,11 +44,11 @@ async def cactpot_task():
 
 
 # 天書奇談 / 老主顧提醒：每週二 15:00
-@tasks.loop(time=time(hour=14, minute=51, tzinfo=TAIWAN_TZ))
+@tasks.loop(time=time(hour=08, minute=34, tzinfo=TAIWAN_TZ))
 async def reset_notice_task():
     now = datetime.now(TAIWAN_TZ)
 
-    if now.weekday() == 2:  # 星期二
+    if now.weekday() == 3:  # 星期二
         print("天書奇談 / 老主顧提醒觸發")
         channel = bot.get_channel(TARGET_CHANNEL_ID)
         if channel:
